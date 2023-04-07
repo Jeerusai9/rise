ebb0deb7919cf6e39b432d3be1e73e6f1f4d23c9
JEERU SAI KRISHNA <84376749+Jeerusai9@users.noreply.github.com>
2023-04-07 02:19:10 -0400

    Update app.py
    modified

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
   
 @app.route('/')
 def hello():
-    return "finally did it after 93 attempts  and succeeded"
+    return "finally did it after 93 attempt"
   
   
 if __name__ == "__main__":
